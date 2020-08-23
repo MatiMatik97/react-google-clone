7f4e4080fd52eb2545f0c81ec99491bb908df403
Mateusz <matimatik97@gmail.com>
2020-08-23 23:26:12 +0200

    DEV-001 - Header and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
-      <h1>Google Clone App</h1>
+      <Router>
+        <Switch>
+          {/* SEARCH PAGE */}
+          <Route path="/search">
+            <h1>Search</h1>
+          </Route>
+          {/* HOME PAGE */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
